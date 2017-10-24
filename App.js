d13dc7786d0951a589f0465c6c8f70a316f5012f
Brian Lusina <lusinabrian@gmail.com>
2017-10-24 07:58:31 +0300

    Update scripts
    postinstall will allow remote debugging the redux store
    remote dev will connect to the local host and port

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Header } from "./src/components";
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Header} from "./src/index";
+import configureStore from "./src/store/configureStore";
+import {Provider} from "react-redux";
+
+const store = configureStore();
+
 
 export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Header/>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <View style={styles.container}>
+                    <Header/>
+                </View>
+            </Provider>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
